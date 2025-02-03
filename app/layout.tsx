2e63d2d9959d17d8c73cd8df2a6efb0edc425055
rafidoth <rafiulhasan803@gmail.com>
2025-02-03 08:59:48 +0600

    autosave

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from '@/app/lib/font'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "rafidoth",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
